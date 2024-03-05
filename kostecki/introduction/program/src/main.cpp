f4b88ce4136d2ce4828aeecd1af876769b08fc48
Mikołaj Kostecki <252942@edu.p.lodz.pl>
2024-03-05 17:50:05 +0100

    Math helper factorial

--- kostecki/introduction/program/src/main.cpp
+++ kostecki/introduction/program/src/main.cpp
@@ -1,11 +1,13 @@
 //Introduction project
 
 #include <iostream>
+#include "math_helpers.h"
 
 using namespace std;
 
 int main()
 {
 	cout << "Hello, World!" << endl;
+	cout << "factorial(5) = " << factorial(5) << endl;
 	return 0;
 }
